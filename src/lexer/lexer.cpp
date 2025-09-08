70e959af4b584a6015209b8d450522b582b5b53a
Gleb Popov <g.popov@innopolis.university>
2025-09-08 18:47:05 +0300

    lexer is ready

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -2,12 +2,113 @@
 
 using namespace std;
 
+static bool checkComments(size_t& i, size_t n, const string& code) {
+    if (code.substr(i, 2) == "//") {
+        while (i < n && code[i] != '\n') {
+            i++;
+        };
+        return true;
+    }
+    return false;
+}
+
+
+static bool checkStringLiterals(size_t& i, size_t n, const string& code, vector<shared_ptr<Token>>& tokens) {
+    if (code[i] == '"') {
+        std::string value;
+        i++;
+        while (i < n && code[i] != '"') {
+            value += code[i];
+            i++;
+        }
+        i++;
+        auto token = make_shared<StringLiteral>();
+        token->type = Token::Type::tkStringLiteral;
+        token->span = {i, value.length()};
+        token->value = value;
+        tokens.push_back(token);
+        return true;
+    }
+    return false;
+}
+
+
+static bool checkNumbers(size_t& i, size_t n, const string& code, vector<shared_ptr<Token>>& tokens) {
+    if (std::isdigit(code[i])) {
+        int start = i;
+        long value = 0;
+        while (i < n && std::isdigit(code[i])) {
+            value = value * 10 + (code[i] - '0');
+            i++;
+        }
+        if (i < n && code[i] == '.') {
+            double realValue = value;
+            double fraction = 0.1;
+            i++;
+            while (i < n && std::isdigit(code[i])) {
+                realValue += (code[i] - '0') * fraction;
+                fraction *= 0.1;
+                i++;
+            }
+            auto token = make_shared<Real>();
+            token->type = Token::Type::tkRealLiteral;
+            token->span = {i, i - start};
+            token->value = value;
+            tokens.push_back(token);
+        } else {
+            auto token = make_shared<Integer>();
+            token->type = Token::Type::tkIntLiteral;
+            token->span = {i, i - start};
+            token->value = value;
+            tokens.push_back(token);
+        }
+        return true;
+    }
+    return false;
+}
+
+
+static bool checkToken(size_t& i, size_t n, const string& code, vector<shared_ptr<Token>>& tokens) {
+    for (const std::pair<std::string, Token::Type>& tok : Token::typeChars) {
+        if (code.substr(i, tok.first.length()) == tok.first) {
+            auto token = make_shared<Token>();
+            token->type = tok.second;
+            token->span = {i, tok.first.length()};
+            tokens.push_back(token);
+            i += tok.first.length();
+            return true;
+        }
+    }
+    return false;
+}
+
+
+static bool checkIdentifier(size_t& i, size_t n, const string& code, vector<shared_ptr<Token>>& tokens) {
+    if (std::isalpha(code[i])) {
+        std::string value;
+        while (i < n && (std::isalnum(code[i]) || code[i] == '_')) {
+            value += code[i];
+            i++;
+        }
+        auto token = make_shared<Identifier>();
+        token->type = Token::Type::tkStringLiteral;
+        token->span = {i, value.length()};
+        token->identifier = value;
+        tokens.push_back(token);
+        return true;
+    }
+    return false;
+}
+
 vector<shared_ptr<Token>> Lexer::tokenize(const string& code) {
     vector<shared_ptr<Token>> tokens;
-    size_t line = 0;
-    size_t position = 0;
     size_t i = 0;
+    size_t n = code.length();
     while (i < code.length()) {
-        
+        if (checkComments(i, n, code)) continue;
+        if (checkStringLiterals(i, n, code, tokens)) continue;
+        if (checkNumbers(i, n, code, tokens)) continue;
+        if (checkToken(i, n, code, tokens)) continue;
     }
+    return tokens;
 }
--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -3,10 +3,10 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <utility>
 
 struct Span {
 public:
-    size_t line;
     size_t position;
     size_t length;
 };
@@ -15,31 +15,6 @@ class Token {
 public: 
     Span span;
     enum class Type {
-        tkVar,
-        tkNewLine,
-        tkSemicolon,
-        tkWhile,
-        tkFor,
-        tkIf,
-        tkThen,
-        tkEnd,
-        tkArrow,
-        tkExit,
-        tkAssign,
-        tkPrint,
-        tkRange,
-        tkIn,
-        tkElse,
-        tkLoop,
-        tkOpenBracket,
-        tkClosedBracket,
-        tkOpenParenthesis,
-        tkClosedParenthesis,
-        tkDot,
-        tkComma,
-        tkAnd,
-        tkOr,
-        tkXor,
         tkGreater,
         tkGreaterEq,
         tkLess,
@@ -50,19 +25,99 @@ public:
         tkMinus,
         tkTimes,
         tkDivide,
+        tkNewLine,
+        tkAssign,
+        tkOpenBracket,
+        tkClosedBracket,
+        tkOpenParenthesis,
+        tkClosedParenthesis,
+        tkOpenCurlyBrace,
+        tkClosedCurlyBrace,
+        tkSemicolon,
+        tkVar,
+        tkWhile,
+        tkFor,
+        tkIf,
+        tkThen,
+        tkEnd,
+        tkArrow,
+        tkExit,
+        tkPrint,
+        tkRange,
+        tkIn,
+        tkElse,
+        tkLoop,
+        tkDot,
+        tkComma,
+        tkAnd,
+        tkOr,
+        tkNot,
+        tkXor,
         tkInt,
         tkReal,
         tkBool,
         tkString,
         tkNone,
         tkFunc,
-        tkOpenCurlyBrace,
-        tkClosedCurlyBrace,
         tkIntLiteral,
         tkRealLiteral,
         tkStringLiteral,
-        tkIdent,
+        tkIdent
     } type;
+
+    static const std::vector<std::pair<std::string, Type>> typeChars;
+};
+
+const std::vector<std::pair<std::string, Token::Type>> Token::typeChars = {
+    std::make_pair("var", Token::Type::tkVar),
+    std::make_pair("while", Token::Type::tkWhile),
+    std::make_pair("for", Token::Type::tkFor),
+    std::make_pair("if", Token::Type::tkIf),
+    std::make_pair("then", Token::Type::tkThen),
+    std::make_pair("end", Token::Type::tkEnd),
+    std::make_pair("exit", Token::Type::tkExit),
+    std::make_pair("print", Token::Type::tkPrint),
+    std::make_pair("in", Token::Type::tkIn),
+    std::make_pair("else", Token::Type::tkElse),
+    std::make_pair("loop", Token::Type::tkLoop),
+    std::make_pair(",", Token::Type::tkComma),
+    std::make_pair("and", Token::Type::tkAnd),
+    std::make_pair("or", Token::Type::tkOr),
+    std::make_pair("not", Token::Type::tkNot),
+    std::make_pair("xor", Token::Type::tkXor),
+    std::make_pair("int", Token::Type::tkInt),
+    std::make_pair("real", Token::Type::tkReal),
+    std::make_pair("string", Token::Type::tkString),
+    std::make_pair("bool", Token::Type::tkBool),
+    std::make_pair("none", Token::Type::tkNone),
+    std::make_pair("func", Token::Type::tkFunc),
+    std::make_pair("+", Token::Type::tkPlus),
+    std::make_pair("-", Token::Type::tkMinus),
+    std::make_pair("*", Token::Type::tkTimes),
+    std::make_pair("\n", Token::Type::tkNewLine),
+    std::make_pair("[", Token::Type::tkOpenBracket),
+    std::make_pair("]", Token::Type::tkClosedBracket),
+    std::make_pair("(", Token::Type::tkOpenParenthesis),
+    std::make_pair(")", Token::Type::tkClosedParenthesis),
+    std::make_pair("{", Token::Type::tkOpenCurlyBrace),
+    std::make_pair("}", Token::Type::tkClosedCurlyBrace),
+    std::make_pair(";", Token::Type::tkSemicolon),
+    std::make_pair(":=", Token::Type::tkAssign),
+
+    std::make_pair("..", Token::Type::tkRange),
+    std::make_pair(".", Token::Type::tkDot),
+
+    std::make_pair("=>", Token::Type::tkArrow),
+    std::make_pair("=", Token::Type::tkEqual),
+
+    std::make_pair(">=", Token::Type::tkGreaterEq),
+    std::make_pair(">", Token::Type::tkGreater),
+
+    std::make_pair("<=", Token::Type::tkLessEq),
+    std::make_pair("<", Token::Type::tkLess),
+
+    std::make_pair("/=", Token::Type::tkNotEqual),
+    std::make_pair("/", Token::Type::tkDivide)
 };
 
 class Identifier : public Token {
@@ -70,17 +125,17 @@ public:
     std::string identifier;
 };
 
-class Integer : Token {
+class Integer : public Token {
 public:
     long value;
 };
 
-class Real : Token {
+class Real : public Token {
 public:
     long double value;
 };
 
-class StringLiteral : Token {
+class StringLiteral : public Token {
 public:
     std::string value;
 };
