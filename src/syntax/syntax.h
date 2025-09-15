6d2cc2328a91800f6ce369d366206b3032cd8044
Gleb Popov <g.popov@innopolis.university>
2025-09-15 17:45:35 +0300

    several classes declared in syntax.h

--- src/syntax/syntax.h
+++ src/syntax/syntax.h
@@ -29,16 +29,15 @@ public:
 
 class Expression;
 
+// Sep -> tkSemicolon | tkNewLine
 bool parseSep(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
               SyntaxErrorReport& report);
 
+// AssignExpression -> tkAssign Expression
 std::optional<std::shared_ptr<Expression>> parseAssignExpression(
     const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
     SyntaxErrorReport& report
 );
-
-bool parseExit(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-               SyntaxErrorReport& report);
 
 // Body -> <* { Statement Sep } *>
 class Body {
@@ -55,7 +54,7 @@ std::optional<std::shared_ptr<Body>> parseLoopBody(const std::vector<std::shared
 
 class Statement {
 public:
-    std::optional<std::shared_ptr<Statement>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
+    static std::optional<std::shared_ptr<Statement>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
                                                    SyntaxErrorReport& report);
     virtual ~Statement() = default;
 };
@@ -66,50 +65,78 @@ public:
     virtual ~VarStatement() override = default;
 };
 
-Statement -> VarStatement // var a := 3
-    | IfStatement         // if a = 3 then ... else ...
-    | ShortIfStatement    // if a = 3 => ...
-    | WhileStatement      // while ... loop ... end
-    | ForStatement        // for i in start..stop loop ... end
-    | ExitStatement       // exit
-    | AssignStatement     // a := 3
-    | PrintStatement      // print a, b, "c"
-    | ReturnStatement     // return a + 4
-    | ExpressionStatement // myObj.method(a, a + 1)
-    | EmptyStatement      // ;
+// Statement -> VarStatement // var a := 3
+//     | IfStatement         // if a = 3 then ... else ...
+//     | ShortIfStatement    // if a = 3 => ...
+//     | WhileStatement      // while ... loop ... end
+//     | ForStatement        // for i in start..stop loop ... end
+//     | ExitStatement       // exit
+//     | AssignStatement     // a := 3
+//     | PrintStatement      // print a, b, "c"
+//     | ReturnStatement     // return a + 4
+//     | ExpressionStatement // myObj.method(a, a + 1)
+//     | EmptyStatement      // ;
 
-VarStatement -> tkVar [tkNewLine] tkIdent [ AssignExpression ]
-    { tkComma [tkNewLine] tkIdent [ AssignExpression ] }
+// VarStatement -> tkVar [tkNewLine] tkIdent [ AssignExpression ]
+//     { tkComma [tkNewLine] tkIdent [ AssignExpression ] }
 
-IfStatement -> tkIf < Expression > tkThen [tkNewLine] Body
-    [ tkElse [tkNewLine] Body ] tkEnd
+// IfStatement -> tkIf < Expression > tkThen [tkNewLine] Body
+//     [ tkElse [tkNewLine] Body ] tkEnd
 
-ShortIfStatement -> tkIf < Expression > tkArrow Statement
+// ShortIfStatement -> tkIf < Expression > tkArrow Statement
 
-WhileStatement -> tkWhile < Expression > LoopBody
+// WhileStatement -> tkWhile < Expression > LoopBody
 
-ForStatement -> tkFor [ Reference tkIn ] < Expression > [ tkRange < Expression > ] LoopBody
+// ForStatement -> tkFor [ Reference tkIn ] < Expression > [ tkRange < Expression > ] LoopBody
 
-AssignStatement -> Reference tkAssign Expression
+// ExitStatement -> tkExit
 
-PrintStatement -> tkPrint [ CommaExpressions ]
+// AssignStatement -> Reference tkAssign Expression
 
-CommaExpressions -> Expression { tkComma Expression }
+// PrintStatement -> tkPrint [ CommaExpressions ]
 
-ReturnStatement -> tkReturn [ Expression ]
+// ReturnStatement -> tkReturn [ Expression ]
 
-ExpressionStatement -> Expression
+// ExpressionStatement -> Expression
 
-EmptyStatement -> 
+// EmptyStatement -> 
 
-Reference -> tkIdent { Accessor }
+// CommaExpressions -> Expression { tkComma Expression }
+class CommaExpressions {
+public:
+    std::vector<std::shared_ptr<Expression>> expressions;
+    std::optional<std::shared_ptr<CommaExpressions>> parse(
+        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
+        SyntaxErrorReport& report
+    );
+};
 
-Accessor -> MemberAccessor | IndexAccessor
+// Accessor -> MemberAccessor | IndexAccessor
+class Accessor {
+public:
+    static std::optional<std::shared_ptr<Accessor>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    virtual ~Accessor() = default;
+};
 
-MemberAccessor -> tkDot ( tkIdent | tkIntLiteral | ParenthesesExpression )
-//                        a.value   a.2            a.(1 + i)
+// MemberAccessor -> tkDot ( tkIdent | tkIntLiteral | ParenthesesExpression )
+//                           a.value   a.2            a.(1 + i)
+class MemberAccessor : public Accessor {
+public:
+    // TODO: attibutes
+    static std::optional<std::shared_ptr<MemberAccessor>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    virtual ~MemberAccessor() override = default;
+};
 
-IndexAccessor -> tkOpenBracket < Expression > tkClosedBracket
+// IndexAccessor -> tkOpenBracket < Expression > tkClosedBracket
+class IndexAccessor : public Accessor {
+public:
+    std::shared_ptr<Expression> expressionInBrackets;
+    static std::optional<std::shared_ptr<IndexAccessor>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    virtual ~IndexAccessor() override = default;
+};
+
+// Reference -> tkIdent { Accessor }
+std::optional<std::shared_ptr<Accessor>> parseReference(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
 
 // Binary operator precedence:
 // 1. * /
@@ -124,35 +151,83 @@ IndexAccessor -> tkOpenBracket < Expression > tkClosedBracket
 // 3. obj is type
 // 4. not value
 
-Expression -> XorOperand { tkXor XorOperand }
-XorOperand -> OrOperand { tkOr OrOperand }
-OrOperand -> AndOperand { tkAnd AndOperand }
-AndOperand -> Sum { BinaryRelation Sum }
-BinaryRelation -> tkLess | tkLessEq
-    | tkGreater | tkGreaterEq
-    | tkEqual | tkNotEqual
-Sum -> Term { (tkPlus | tkMinus) Term }
-Term -> Unary { (tkTimes | tkDivide) Unary }
+// Expression -> XorOperand { tkXor XorOperand }
+// XorOperand -> OrOperand { tkOr OrOperand }
+// OrOperand -> AndOperand { tkAnd AndOperand }
+// AndOperand -> Sum { BinaryRelation Sum }
+// BinaryRelation -> tkLess | tkLessEq
+//     | tkGreater | tkGreaterEq
+//     | tkEqual | tkNotEqual
+// Sum -> Term { (tkPlus | tkMinus) Term }
+// Term -> Unary { (tkTimes | tkDivide) Unary }
 
-Unary -> {tkNot} TypecheckUnary
-TypecheckUnary -> MinusPlusUnary { tkIs TypeId }
-TypeId -> tkInt | tkReal | tkString | tkBool | tkNone | tkFunc
-    | tkOpenBracket tkClosedBracket | tkOpenCurlyBrace tkClosedCurlyBrace
-MinusPlusUnary -> {tkPlus | tkMinus} AccessorCallUnary
-AccessorCallUnary -> Primary { Accessor | Call }
-Call -> tkOpenParenthesis < [ CommaExpressions ] > tkClosedParenthesis
+// Unary -> {tkNot} TypecheckUnary
+// TypecheckUnary -> MinusPlusUnary { tkIs TypeId }
+// TypeId -> tkInt | tkReal | tkString | tkBool | tkNone | tkFunc
+//     | tkOpenBracket tkClosedBracket | tkOpenCurlyBrace tkClosedCurlyBrace
+// MinusPlusUnary -> {tkPlus | tkMinus} AccessorCallUnary
+// AccessorCallUnary -> Primary { Accessor | Call }
+// Call -> tkOpenParenthesis < [ CommaExpressions ] > tkClosedParenthesis
 
-Primary -> PrimaryIdent | ParenthesesExpression | FuncLiteral | TokenLiteral | ArrayLiteral | TupleLiteral
-PrimaryIdent -> tkIdent
-ParenthesesExpression -> tkOpenParenthesis < Expression > tkClosedParenthesis
-TokenLiteral -> tkStringLiteral | tkIntLiteral | tkRealLiteral | tkTrue | tkFalse | tkNone
-FuncLiteral -> tkFunc tkOpenParenthesis < [ CommaIdents ] > tkClosedParenthesis FuncBody
-ArrayLiteral -> tkOpenBracket < [ CommaExpressions ] > tkClosedBracket
-TupleLiteral -> tkOpenCurlyBrace < [ TupleLiteralElement { tkComma TupleLiteralElement } ] > tkClosedCurlyBrace
-TupleLiteralElement -> [ tkIdent tkAssign ] Expression
-FuncBody -> ShortFuncBody | LongFuncBody
-ShortFuncBody -> tkArrow Expression
-LongFuncBody -> tkIs Body tkEnd
+// Primary -> PrimaryIdent | ParenthesesExpression | FuncLiteral | TokenLiteral | ArrayLiteral | TupleLiteral
+// PrimaryIdent -> tkIdent
+// ParenthesesExpression -> tkOpenParenthesis < Expression > tkClosedParenthesis
+// TokenLiteral -> tkStringLiteral | tkIntLiteral | tkRealLiteral | tkTrue | tkFalse | tkNone
+// FuncLiteral -> tkFunc tkOpenParenthesis < [ CommaIdents ] > tkClosedParenthesis FuncBody
+
+// ArrayLiteral -> tkOpenBracket < [ CommaExpressions ] > tkClosedBracket
+class ArrayLiteral {
+public:
+    std::optional<std::shared_ptr<CommaExpressions>> commaExpression;
+    std::optional<std::shared_ptr<ArrayLiteral>> parse(
+        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
+        SyntaxErrorReport& report
+    );
+};
+
+// TupleLiteralElement -> [ tkIdent tkAssign ] Expression
+class TupleLiteralElement {
+public:
+    std::optional<std::shared_ptr<IdentifierToken>> ident;
+    std::shared_ptr<Expression> expression;
+    std::optional<std::shared_ptr<TupleLiteralElement>> parse(
+        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
+        SyntaxErrorReport& report
+    );
+};
+
+// TupleLiteral -> tkOpenCurlyBrace < [ TupleLiteralElement { tkComma TupleLiteralElement } ] > tkClosedCurlyBrace
+class TupleLiteral {
+public:
+    std::vector<std::shared_ptr<TupleLiteralElement>> elements;
+    std::optional<std::shared_ptr<TupleLiteral>> parse(
+        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
+        SyntaxErrorReport& report
+    );
+};
+
+// FuncBody -> ShortFuncBody | LongFuncBody
+class FuncBody {
+public:
+    static std::optional<std::shared_ptr<FuncBody>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    virtual ~FuncBody() = default;
+};
+
+// ShortFuncBody -> tkArrow Expression
+class ShortFuncBody : public FuncBody {
+public:
+    std::shared_ptr<Expression> expressionToReturn;
+    static std::optional<std::shared_ptr<ShortFuncBody>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    virtual ~ShortFuncBody() override = default;
+};
+
+// LongFuncBody -> tkIs Body tkEnd
+class LongFuncBody : public FuncBody {
+public:
+    std::shared_ptr<Body> funcBody;
+    static std::optional<std::shared_ptr<LongFuncBody>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    virtual ~LongFuncBody() override = default;
+};
 
 }
 
