56733ea2c2cb17c6d4e785fa198c0591eb6f2bd2
Timur Usmanov <t.usmanov@innopolis.university>
2025-09-15 17:53:29 +0300

    Introduce SyntaxContext

--- src/syntax/syntax.h
+++ src/syntax/syntax.h
@@ -11,14 +11,14 @@ class SyntaxErrorReport {
 public:
     size_t rightmostPos = 0;
     std::vector<std::shared_ptr<complog::CompilationMessage>> messages;
-    void Report(size_t pos, const std::shared_ptr<complog::CompilationMessage>& msg);
+    void Report(size_t& pos, const std::shared_ptr<complog::CompilationMessage>& msg);
 };
 
 struct SyntaxContext {
     const std::vector<std::shared_ptr<Token>> tokens;
     SyntaxErrorReport report;
     std::shared_ptr<const locators::CodeFile> file;
-    locators::Locator MakeLocator(size_t pos) const;
+    locators::Locator MakeLocator(size_t& pos) const;
 };
 
 namespace ast {
@@ -27,11 +27,7 @@ class Statement;
 class Program {
 public:
     std::vector<std::shared_ptr<Statement>> statements;
-    static std::optional<std::shared_ptr<Program>> parse(
-        const std::vector<std::shared_ptr<Token>>& tokens,
-        const std::shared_ptr<const locators::CodeFile>& file,
-        complog::ICompilationLog& log
-    );
+    static std::optional<std::shared_ptr<Program>> parse(SyntaxContext& context, size_t& pos);
 };
 
 class Expression;
@@ -41,28 +37,20 @@ bool parseSep(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
               SyntaxErrorReport& report);
 
 // AssignExpression -> tkAssign Expression
-std::optional<std::shared_ptr<Expression>> parseAssignExpression(
-    const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-    SyntaxErrorReport& report
-);
+std::optional<std::shared_ptr<Expression>> parseAssignExpression(SyntaxContext& context, size_t& pos);
 
 // Body -> <* { Statement Sep } *>
 class Body {
 public:
     std::vector<std::shared_ptr<Statement>> statements;
-    std::optional<std::shared_ptr<Body>> parse(
-        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-        SyntaxErrorReport& report
-    );
+    std::optional<std::shared_ptr<Body>> parse(SyntaxContext& context, size_t& pos);
 };
 
-std::optional<std::shared_ptr<Body>> parseLoopBody(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-                                                   SyntaxErrorReport& report);
+std::optional<std::shared_ptr<Body>> parseLoopBody(SyntaxContext& context, size_t& pos);
 
 class Statement {
 public:
-    static std::optional<std::shared_ptr<Statement>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-                                                   SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<Statement>> parse(SyntaxContext& context, size_t& pos);
     virtual ~Statement() = default;
 };
 
@@ -112,16 +100,13 @@ public:
 class CommaExpressions {
 public:
     std::vector<std::shared_ptr<Expression>> expressions;
-    std::optional<std::shared_ptr<CommaExpressions>> parse(
-        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-        SyntaxErrorReport& report
-    );
+    std::optional<std::shared_ptr<CommaExpressions>> parse(SyntaxContext& context, size_t& pos);
 };
 
 // Accessor -> MemberAccessor | IndexAccessor
 class Accessor {
 public:
-    static std::optional<std::shared_ptr<Accessor>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<Accessor>> parse(SyntaxContext& context, size_t& pos);
     virtual ~Accessor() = default;
 };
 
@@ -130,7 +115,7 @@ public:
 class MemberAccessor : public Accessor {
 public:
     // TODO: attibutes
-    static std::optional<std::shared_ptr<MemberAccessor>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<MemberAccessor>> parse(SyntaxContext& context, size_t& pos);
     virtual ~MemberAccessor() override = default;
 };
 
@@ -138,12 +123,12 @@ public:
 class IndexAccessor : public Accessor {
 public:
     std::shared_ptr<Expression> expressionInBrackets;
-    static std::optional<std::shared_ptr<IndexAccessor>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<IndexAccessor>> parse(SyntaxContext& context, size_t& pos);
     virtual ~IndexAccessor() override = default;
 };
 
 // Reference -> tkIdent { Accessor }
-std::optional<std::shared_ptr<Accessor>> parseReference(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+std::optional<std::shared_ptr<Accessor>> parseReference(SyntaxContext& context, size_t& pos);
 
 // Binary operator precedence:
 // 1. * /
@@ -186,10 +171,7 @@ std::optional<std::shared_ptr<Accessor>> parseReference(const std::vector<std::s
 class ArrayLiteral {
 public:
     std::optional<std::shared_ptr<CommaExpressions>> commaExpression;
-    std::optional<std::shared_ptr<ArrayLiteral>> parse(
-        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-        SyntaxErrorReport& report
-    );
+    std::optional<std::shared_ptr<ArrayLiteral>> parse(SyntaxContext& context, size_t& pos);
 };
 
 // TupleLiteralElement -> [ tkIdent tkAssign ] Expression
@@ -197,26 +179,20 @@ class TupleLiteralElement {
 public:
     std::optional<std::shared_ptr<IdentifierToken>> ident;
     std::shared_ptr<Expression> expression;
-    std::optional<std::shared_ptr<TupleLiteralElement>> parse(
-        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-        SyntaxErrorReport& report
-    );
+    std::optional<std::shared_ptr<TupleLiteralElement>> parse(SyntaxContext& context, size_t& pos);
 };
 
 // TupleLiteral -> tkOpenCurlyBrace < [ TupleLiteralElement { tkComma TupleLiteralElement } ] > tkClosedCurlyBrace
 class TupleLiteral {
 public:
     std::vector<std::shared_ptr<TupleLiteralElement>> elements;
-    std::optional<std::shared_ptr<TupleLiteral>> parse(
-        const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos,
-        SyntaxErrorReport& report
-    );
+    std::optional<std::shared_ptr<TupleLiteral>> parse(SyntaxContext& context, size_t& pos);
 };
 
 // FuncBody -> ShortFuncBody | LongFuncBody
 class FuncBody {
 public:
-    static std::optional<std::shared_ptr<FuncBody>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<FuncBody>> parse(SyntaxContext& context, size_t& pos);
     virtual ~FuncBody() = default;
 };
 
@@ -224,7 +200,7 @@ public:
 class ShortFuncBody : public FuncBody {
 public:
     std::shared_ptr<Expression> expressionToReturn;
-    static std::optional<std::shared_ptr<ShortFuncBody>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<ShortFuncBody>> parse(SyntaxContext& context, size_t& pos);
     virtual ~ShortFuncBody() override = default;
 };
 
@@ -232,7 +208,7 @@ public:
 class LongFuncBody : public FuncBody {
 public:
     std::shared_ptr<Body> funcBody;
-    static std::optional<std::shared_ptr<LongFuncBody>> parse(const std::vector<std::shared_ptr<Token>>& tokens, size_t& pos, SyntaxErrorReport& report);
+    static std::optional<std::shared_ptr<LongFuncBody>> parse(SyntaxContext& context, size_t& pos);
     virtual ~LongFuncBody() override = default;
 };
 
