cfbb2594a7f42a1307a6f6332c935bfc18899601
Timur Usmanov <t.usmanov@innopolis.university>
2025-09-15 16:49:11 +0300

    WIP syntax header

--- src/syntax/syntax.cpp
+++ src/syntax/syntax.cpp
@@ -1,1 +1,12 @@
- 
+#include "syntax.h"
+using namespace std;
+
+namespace ast {
+
+bool parseSep(const vector<shared_ptr<Token>>& tokens, size_t& pos) {
+    size_t n = tokens.size();
+    if (pos >= n) return false;
+
+}
+
+}
--- src/syntax/syntax.h
+++ src/syntax/syntax.h
@@ -3,10 +3,33 @@
 #include <optional>
 #include <vector>
 #include <memory>
+#include "complog/CompilationLog.h"
+#include "complog/CompilationMessage.h"
+#include "lexer.h"
+
+class SyntaxErrorReport {
+public:
+    size_t rightmostPos = 0;
+    std::vector<std::shared_ptr<complog::CompilationMessage>> messages;
+    void Report(size_t pos, const std::shared_ptr<complog::CompilationMessage>& msg);
+};
+
+namespace ast {
+class Statement;
+
+class Program {
+public:
+    std::vector<std::shared_ptr<Statement>> statements;
+    static std::optional<std::shared_ptr<Program>> Parse(
+        const std::vector<std::shared_ptr<Token>>& tokens,
+        const std::shared_ptr<const locators::CodeFile>& file,
+        complog::ICompilationLog& log);
+};
+}
 
 class SyntaxAnalyzer {
 public:
-    static std::optional<std::shared_ptr<AST::Program>> analyze(
+    static std::optional<std::shared_ptr<ast::Program>> analyze(
         const std::vector<std::shared_ptr<Token>>& tokens,
         const std::shared_ptr<const locators::CodeFile>& file,
         complog::ICompilationLog& log);
