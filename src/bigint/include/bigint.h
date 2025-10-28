6dbf10e05b62fd6f131078775e56fbfb48fff729
Timur Usmanov <t.usmanov@innopolis.university>
2025-10-28 15:56:04 +0300

    Declare bigint

---
+++ src/bigint/include/bigint.h
@@ -0,0 +1,36 @@
+#include <cstdint>
+#include <vector>
+#include <string>
+
+class BigInt {
+private:
+    std::vector<std::uint32_t> v;
+    bool sign;
+public:
+    BigInt(long val);
+    BigInt();
+    BigInt(const std::string& repr, size_t base);
+    BigInt(const std::vector<size_t>& bigEndianRepr, size_t base);
+    BigInt(const BigInt& other) = default;
+    BigInt(BigInt&& other) = default;
+    BigInt& operator=(long val);
+    BigInt& operator=(const BigInt& other) = default;
+    BigInt& operator=(BigInt&& other) = default;
+    std::vector<size_t> Repr(size_t base);
+    std::string ToString(size_t base = 10);
+    BigInt& operator+=(const BigInt& other);
+    BigInt operator+(const BigInt& other) const;
+    BigInt& operator-=(const BigInt& other);
+    BigInt operator-(const BigInt& other) const;
+    BigInt& operator*=(const BigInt& other);
+    BigInt operator*(const BigInt& other) const;
+    BigInt& operator/=(const BigInt& other);
+    BigInt operator/(const BigInt& other) const;
+    BigInt& operator%=(const BigInt& other);
+    BigInt operator%(const BigInt& other) const;
+    std::pair<BigInt, BigInt> DivMod(const BigInt& other) const;
+    int operator<=>(const BigInt& other) const;
+    int operator<=>(long other) const;
+};
+
+int operator<=>(long a, const BigInt& b);
