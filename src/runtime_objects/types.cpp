87c683aba98275b739da3eaecdbc3b50baf2ab9e
Timur Usmanov <t.usmanov@innopolis.university>
2025-11-02 15:52:17 +0300

    Implement IntegerType

--- src/runtime_objects/types.cpp
+++ src/runtime_objects/types.cpp
@@ -9,16 +9,89 @@ using namespace std;
 
 namespace runtime {
 
-// Implement class Type
+std::optional<std::shared_ptr<Type>> Type::BinaryPlus(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::BinaryMinus(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::BinaryMul(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::BinaryDiv(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::BinaryLogical(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::BinaryEq(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::BinaryOrdering(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::UnaryMinus(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::UnaryPlus(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::UnaryNot(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::Field(const std::string& name) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::Field(const Type& other) const { return {}; }
+std::optional<std::shared_ptr<Type>> Type::Subscript(const Type& other) const { return {}; }
 
-// Implement class IntegerType
-/*
- * Has fields:
- * + Round: int = this
- * + Floor: int = this
- * + Ceil: int = this
- * + Frac: real = 0.0
- */
+bool IntegerType::TypeEq(const Type& other) const {
+    return !!dynamic_cast<const IntegerType*>(&other);
+}
+
+std::string IntegerType::Name() const {
+    return "int";
+}
+
+static bool IsRealOrInt(const Type& t) {
+    return t.TypeEq(IntegerType()) || t.TypeEq(RealType());
+}
+
+static optional<shared_ptr<Type>> NumericArith(const Type& a, const Type& b) {
+    auto inttype = make_shared<IntegerType>();
+    auto realtype = make_shared<RealType>();
+    bool inta, intb;
+    if (a.TypeEq(*inttype)) inta = true;
+    else if (a.TypeEq(*realtype)) inta = false;
+    else return {};
+    if (b.TypeEq(*inttype)) intb = true;
+    else if (b.TypeEq(*realtype)) intb = false;
+    else return {};
+    bool intres = inta && intb;
+    return intres ? static_cast<shared_ptr<Type>>(inttype) : realtype;
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::BinaryPlus(const Type& other) const {
+    return NumericArith(*this, other);
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::BinaryMinus(const Type& other) const {
+    return NumericArith(*this, other);
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::BinaryMul(const Type& other) const {
+    return NumericArith(*this, other);
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::BinaryDiv(const Type& other) const {
+    return NumericArith(*this, other);
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::BinaryEq(const Type& other) const {
+    if (IsRealOrInt(other)) return make_shared<BoolType>();
+    return {};
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::BinaryOrdering(const Type& other) const {
+    if (IsRealOrInt(other)) return make_shared<BoolType>();
+    return {};
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::UnaryMinus(const Type& other) const {
+    return make_shared<IntegerType>();
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::UnaryPlus(const Type& other) const {
+    return make_shared<IntegerType>();
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::Field(const std::string& name) const {
+    if (name == "Round" || name == "Floor" || name == "Ceil") return make_shared<IntegerType>();
+    if (name == "Frac") return make_shared<RealType>();
+    return {};
+}
+
+std::optional<std::shared_ptr<Type>> IntegerType::Field(const Type& other) const {
+    return {};
+}
 
 // Implement class RealType
 /*
