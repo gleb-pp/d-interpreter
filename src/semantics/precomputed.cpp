2078202a8561b2f087d6bb60541375e2b63a5744
Timur Usmanov <t.usmanov@innopolis.university>
2025-11-03 15:07:04 +0300

    Define the precomputed ast node

---
+++ src/semantics/precomputed.cpp
@@ -0,0 +1,12 @@
+#include "precomputed.h"
+using namespace std;
+
+namespace ast {
+
+PrecomputedValue::PrecomputedValue(const locators::SpanLocator& pos, const std::shared_ptr<runtime::RuntimeValue>& val)
+    : Expression(pos), Value(val) {}
+void PrecomputedValue::AcceptVisitor(IASTVisitor& vis) {
+    vis.VisitCustom(*this);
+}
+
+}
