79babe3070d68cf3099e260a2a71bfb2b6098817
Timur Usmanov <t.usmanov@innopolis.university>
2025-11-15 17:38:28 +0300

    Declare interp::Variable

---
+++ src/interp/include/interp/variable.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <memory>
+#include <string>
+#include "runtime.h"
+#include "runtime/values.h"
+
+namespace interp {
+
+class Variable : public std::enable_shared_from_this<Variable> {
+    std::string name;
+    std::shared_ptr<runtime::RuntimeValue> val;
+
+public:
+    Variable(const std::shared_ptr<runtime::RuntimeValue>& content);
+    Variable();  // contains None
+    const std::string& Name() const;
+    void Assign(const std::shared_ptr<runtime::RuntimeValue>& content);
+    const std::shared_ptr<runtime::RuntimeValue>& Content() const;
+};
+
+}
