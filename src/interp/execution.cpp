d7e93316575328a69d8d58f7e6d8ca215a7dda3e
Timur Usmanov <t.usmanov@innopolis.university>
2025-11-17 12:09:47 +0300

    Implement for, loop, exit; WIP AssignStatement

--- src/interp/execution.cpp
+++ src/interp/execution.cpp
@@ -1,4 +1,6 @@
 #include "interp/execution.h"
+#include <algorithm>
+#include <ios>
 #include <memory>
 #include "interp/runtimeContext.h"
 #include "runtime/derror.h"
@@ -98,19 +100,145 @@ void Executor::VisitWhileStatement(ast::WhileStatement& node) {
 }
 
 void Executor::VisitForStatement(ast::ForStatement& node) {
-    // TODO
+    optional<shared_ptr<Variable>> cyclevar;
+    if (node.optVariableName)
+        cyclevar = make_shared<Variable>(node.optVariableName.value()->identifier, make_shared<runtime::NoneValue>());
+
+    shared_ptr<runtime::RuntimeValue> startOrList;
+    {
+        auto optStartOrList = ExecuteExpressionInThis(node.startOrList);
+        if (!optStartOrList) return;
+        startOrList = *optStartOrList;
+    }
+    optional<variant<pair<BigInt, BigInt>, vector<shared_ptr<runtime::RuntimeValue>>, size_t>> range;
+    if (node.end) {
+        auto intstart = dynamic_pointer_cast<runtime::IntegerValue>(startOrList);
+        if (!intstart) {
+            context.SetThrowingState(
+                runtime::DRuntimeError("Starting bound was of type \"" + startOrList->TypeOfValue()->Name() +
+                                       "\", expected an integer"), node.startOrList->pos);
+            return;
+        }
+        auto end = ExecuteExpressionInThis(*node.end);
+        if (!end) return;
+        auto intend = dynamic_pointer_cast<runtime::IntegerValue>(*end);
+        if (!intend) {
+            context.SetThrowingState(
+                runtime::DRuntimeError("Ending bound was of type \"" + end.value()->TypeOfValue()->Name() +
+                                       "\", expected an integer"), node.end.value()->pos);
+            return;
+        }
+        range.emplace(make_pair(intstart->Value(), intend->Value()));
+    } else {
+        auto arrayval = dynamic_pointer_cast<runtime::ArrayValue>(startOrList);
+        if (arrayval) {
+            if (cyclevar) {
+                range.emplace(vector<shared_ptr<runtime::RuntimeValue>>());
+                ranges::transform(arrayval->Value, get<1>(*range).begin(),
+                                  [](const pair<const BigInt, shared_ptr<runtime::RuntimeValue>>& p) { return p.second; });
+            } else range.emplace(arrayval->Value.size());
+        }
+        else {
+            auto tupleval = dynamic_pointer_cast<runtime::TupleValue>(startOrList);
+            if (tupleval) {
+                if (cyclevar) range.emplace(tupleval->Values());
+                else range.emplace(tupleval->Values().size());
+            }
+            else {
+                context.SetThrowingState(
+                    runtime::DRuntimeError("Expected an iterable type (array or tuple), but got \"" +
+                                           startOrList->TypeOfValue()->Name()),
+                    node.startOrList->pos);
+                return;
+            }
+        }
+    }
+
+    auto prevScopes = scopes;
+    if (cyclevar) {
+        scopes = make_shared<ScopeStack>(scopes);
+        scopes->Declare(*cyclevar);
+    }
+    switch (range->index()) {
+        case 0: {  // range from BigInt to BigInt
+            auto& [start, end] = get<0>(*range);
+            bool decrement = start > end;
+            auto cur = start;
+            while (true) {
+                if (cyclevar) cyclevar.value()->Assign(make_shared<runtime::IntegerValue>(cur));
+                VisitBody(*node.action);
+                if (!context.State.IsRunning()) {
+                    if (context.State.IsExiting()) context.State = RuntimeState::Running();
+                    break;
+                }
+                if (cur == end) break;
+                if (decrement) --cur;
+                else ++cur;
+            }
+            break;
+        }
+        case 1: {  // using a cycle variable, iterating over a collection
+            auto& items = get<1>(*range);
+            for (auto& item : items) {
+                cyclevar.value()->Assign(item);
+                VisitBody(*node.action);
+                if (!context.State.IsRunning()) {
+                    if (context.State.IsExiting()) context.State = RuntimeState::Running();
+                    break;
+                }
+            }
+            break;
+        }
+        case 2: {  // iterating several times without a variable
+            size_t n = get<2>(*range);
+            for (size_t i = 0; i < n; ++i) {
+                VisitBody(*node.action);
+                if (!context.State.IsRunning()) {
+                    if (context.State.IsExiting()) context.State = RuntimeState::Running();
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    scopes = prevScopes;
 }
 
 void Executor::VisitLoopStatement(ast::LoopStatement& node) {
-    // TODO
+    while (true) {
+        VisitBody(*node.body);
+        if (!context.State.IsRunning()) {
+            if (context.State.IsExiting()) context.State = RuntimeState::Running();
+            break;
+        }
+    }
 }
 
 void Executor::VisitExitStatement(ast::ExitStatement& node) {
-    // TODO
+    context.State = RuntimeState::Exiting();
 }
 
 void Executor::VisitAssignStatement(ast::AssignStatement& node) {
-    // TODO
+    shared_ptr<runtime::RuntimeValue> val;
+    {
+        auto optVal = ExecuteExpressionInThis(node.src);
+        if (!optVal) return;
+        val = *optVal;
+    }
+    auto optvariable = scopes->Lookup(node.dest->baseIdent->identifier);
+    if (!optvariable) {
+        auto span = node.dest->baseIdent->span;
+        context.SetThrowingState(
+            runtime::DRuntimeError("Variable not declared: \"" + node.dest->baseIdent->identifier + "\""),
+            locators::SpanLocator(node.pos.File(), span.position, span.length));
+        return;
+    }
+    if (node.dest->accessorChain.empty()) {
+        optvariable.value()->Assign(val);
+        return;
+    }
+    size_t n = node.dest->accessorChain.size() - 1;
+
 }
 
 void Executor::VisitPrintStatement(ast::PrintStatement& node) {
