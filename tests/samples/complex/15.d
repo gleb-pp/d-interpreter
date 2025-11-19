0b054b431034734b46693983d097808a6bb0863b
Timur Usmanov <t.usmanov@innopolis.university>
2025-11-19 11:11:20 +0300

    Update test samples

--- tests/samples/complex/15.d
+++ tests/samples/complex/15.d
@@ -1,6 +1,8 @@
 var list := ["a", "b", "c"]
 var tuple := {name := "Java", surname := "Script"}
 
+func() is print; end () // make all unknown
+
 print list[0] is none  // true
 print list[4] is none  // true
 print list[-1] is none // true
